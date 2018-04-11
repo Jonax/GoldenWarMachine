0d914a22542dc315b2b87c2b9494332a624aae14
Jon Wills <git@chromaticaura.net>
2018-04-11 04:33:53 +0100

    Customised CSS pipelines
    Because CSS output has varied wildly from four months ago, it seems important to more closely lock down the pipeline.
    
    Added more explicit configuration for CSS processing, as well as a switch to toggle comments between versions.

--- setup/gulpfile.babel.js
+++ setup/gulpfile.babel.js
@@ -187,6 +187,33 @@ function CSSPipe(project, deployment, splitMode, file = undefined)
 		}
 	}
 
+	let level = {
+		1: {
+			selectorsSortingMethod: "natural",
+			replaceZeroUnits: true
+		},
+		2: {
+			mergeAdjacentRules: true, // controls adjacent rules merging; defaults to true
+			mergeIntoShorthands: true, // controls merging properties into shorthands; defaults to true
+			mergeMedia: true, // controls `@media` merging; defaults to true
+			mergeNonAdjacentRules: true, // controls non-adjacent rule merging; defaults to true
+			mergeSemantically: true, // controls semantic merging; defaults to false
+			overrideProperties: true, // controls property overriding based on understandability; defaults to true
+			removeEmpty: true, // controls removing empty rules and nested blocks; defaults to `true`
+			reduceNonAdjacentRules: true, // controls non-adjacent rule reducing; defaults to true
+			removeDuplicateFontRules: true, // controls duplicate `@font-face` removing; defaults to true
+			removeDuplicateMediaBlocks: true, // controls duplicate `@media` removing; defaults to true
+			removeDuplicateRules: true, // controls duplicate rules removing; defaults to true
+			removeUnusedAtRules: true, // controls unused at rule removing; defaults to false (available since 4.1.0)
+			restructureRules: true, // controls rule restructuring; defaults to false
+			skipProperties: [] // controls which properties won't be optimized, defaults to `[]` which means all will be optimized (since 4.1.0)
+		}
+	}
+	if (deployment.comments)
+	{
+		level = 0;
+	}
+
 	const sourceFile = file || `${dirs.src}/${project.name}/css/main.css`;
 	return gulp.src(sourceFile)
 				.pipe(gulp_if(options.verbose, using()))
@@ -201,28 +228,7 @@ function CSSPipe(project, deployment, splitMode, file = undefined)
 					}),
 				]))
 				.pipe(cleancss({
-					level: {
-						1: {
-							selectorsSortingMethod: "natural",
-							replaceZeroUnits: true
-						},
-						2: {
-							mergeAdjacentRules: true, // controls adjacent rules merging; defaults to true
-							mergeIntoShorthands: true, // controls merging properties into shorthands; defaults to true
-							mergeMedia: true, // controls `@media` merging; defaults to true
-							mergeNonAdjacentRules: true, // controls non-adjacent rule merging; defaults to true
-							mergeSemantically: true, // controls semantic merging; defaults to false
-							overrideProperties: true, // controls property overriding based on understandability; defaults to true
-							removeEmpty: true, // controls removing empty rules and nested blocks; defaults to `true`
-							reduceNonAdjacentRules: true, // controls non-adjacent rule reducing; defaults to true
-							removeDuplicateFontRules: true, // controls duplicate `@font-face` removing; defaults to true
-							removeDuplicateMediaBlocks: true, // controls duplicate `@media` removing; defaults to true
-							removeDuplicateRules: true, // controls duplicate rules removing; defaults to true
-							removeUnusedAtRules: true, // controls unused at rule removing; defaults to false (available since 4.1.0)
-							restructureRules: true, // controls rule restructuring; defaults to false
-							skipProperties: [] // controls which properties won't be optimized, defaults to `[]` which means all will be optimized (since 4.1.0)
-						}
-					},
+					level: level,
 					format: format
 				}));
 }
